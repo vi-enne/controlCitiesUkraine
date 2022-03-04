00dde3b665f4420b0b5ea5dcc00ac5e4037194ae
vi-enne <vittorio.nicoletta.1@ulaval.ca>
2022-03-04 12:34:26 -0500

    Update controlCitiesUkraine.R

--- controlCitiesUkraine.R
+++ controlCitiesUkraine.R
@@ -59,7 +59,7 @@ write.csv(df, paste0("output/Cities_and_towns_during_the_Russo-Ukrainian_War_", 
 total <- read.csv("https://raw.githubusercontent.com/vi-enne/controlCitiesUkraine/main/output/Cities_and_towns_during_the_Russo-Ukrainian_War_total.csv", 
                   fileEncoding = "UTF-8", sep = ",", check.names=FALSE)
 if(df$update[nrow(df)] != total$update[nrow(total)]){
- df <- rbind(df, total)
+ df <- rbind(total, df)
  df[is.na(df)] <- ""
  write.csv(df, paste0("output/Cities_and_towns_during_the_Russo-Ukrainian_War_", "total", ".csv"),
            row.names = F,

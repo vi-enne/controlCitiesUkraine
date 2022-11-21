33d8454082fd455dee48e8897f7bbcd4fcea2214
vi-enne <44818674+vi-enne@users.noreply.github.com>
2022-11-21 11:57:16 -0500

    Update controlCitiesUkraine.R

--- controlCitiesUkraine.R
+++ controlCitiesUkraine.R
@@ -99,7 +99,8 @@ df$NameSimple <- df$Name
 df$NameSimple <- gsub("[^[:alpha:]]", "", df$NameSimple)
 #Fix cities with same name
 sameName <- c("Shevchenkove", "Ukrainka", "Troitske", "Kamianske", "Rubizhne", "Petropavlivka", "Novoselivka",
-             "Blahodatne", "Makiivka", "Mykhailivka", "Novomykolaivka", "Novopetrivka", "Oleksandrivka", "Vasylivka")
+             "Blahodatne", "Makiivka", "Mykhailivka", "Novomykolaivka", "Novopetrivka", "Oleksandrivka", "Vasylivka",
+             "Pokrovske")
 for (i in 1:length(sameName)){
   df$NameSimple[df$NameSimple==sameName[i]] <- paste(df$NameSimple[df$NameSimple==sameName[i]],
                                                      df$Raion[df$NameSimple==sameName[i]],
